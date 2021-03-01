bd4d235ca95d0cf65afd024fbc93a2e1a52d4b01
wkraam <wkraam@yahoo.com>
2021-03-01 11:45:41 +0200

    tuöeb kodus edasi teha

--- Prax2/WaltherKraamPrax2.2server.py
+++ Prax2/WaltherKraamPrax2.2server.py
@@ -3,6 +3,39 @@ import socket
 import sys
 from _thread import *
 
+class Blinker:  #ülesanne 1.2 prax 1
+	def __init__(self, sleepTime = 0.5):
+		try:
+			import RPi.GPIO as GPIO
+		except RuntimeError:
+			print("...error importing RPi.GPIO module")
+		print("Blink initialized")
+		self.sleepTime = sleepTime
+		self.GPIO = GPIO
+
+	def setSleepTime(self, time):
+		self.sleepTime = time
+		print("Set sleepTime to: "+str(self.sleepTime))
+
+	def getSleepTime(self):
+		print(self.sleepTime)
+
+	def main(self):
+		from time import sleep
+
+		self.GPIO.setmode(GPIO.BOARD)
+		pins=[36, 38]
+		self.GPIO.setup(pins, GPIO.OUT)
+		self.GPIO.output(36, 1)
+		self.GPIO.output(38, 0)
+		sleep(self.sleepTime)
+		self.GPIO.output(36, 0)
+		self.GPIO.output(38, 1)
+		sleep(self.sleepTime)
+		print(self.sleepTime)
+	def stop(self):
+		self.GPIO.cleanup(pins)
+Blinker = Blinker()
 listenPort = input("Enter the port to listen to: ")
 HOST = ''
 if listenPort != "":
@@ -31,20 +64,39 @@ print('---Socket is now listening---')
 
 #fn for handling connections... this will be used to create threads
 def clientthread(conn):
+	eelnevData = ""
 	#sending messages to connected clients
 	conn.send('willkommen auf meinem server, kirjuta midagi ja vajuta enter... \n'.encode()) #send only takses bytes
 	while True:
-		try:
-			#recive data from client
+		
+		try:	#recive data from client
+			
+			if eelnevData == "start":
+				Blinker.main()
+
+			elif eelnevData == "stop":
+				Blinker.stop()
+
+			print("start of line")
 			data = conn.recv(buffer_size)
 			if not data:
+				print("---breaking connection---")
 				break
+			sisenevData = data.decode()
+			if sisenevData == "start" or "stop":
+				eelnevData = sisenevData
+			if str(sisenevData).isnumeric():
+				print("isnumeric!")
+				Blinker.setSleepTime(int(sisenevData))
+			if sisenevData[:2] == "0.":
+				if sisenevData[2:].isnumeric():
+					print("isnumeric!")
+					Blinker.setSleepTime(float("0."+sisenevData[2:]))
 			reply = '---OK---' + data.decode() #encode bytes to string
 			conn.sendall(reply.encode())
-			print('recived: ')
-			print(data.decode())
-			print('sent: ')
-			print(reply)
+			print('recived: '+data.decode())
+			print('replied: '+reply)
+			print()
 		except socket.error as message:
 			print(message)
 			break
--- Prax2/prax1_12.py
+++ Prax2/prax1_12.py
@@ -1,21 +1,38 @@
 #!/usr/bin/python3
-try:
-	import RPi.GPIO as GPIO
-except RuntimeError:
-	print("...error importing RPi.GPIO module")
+class Blinker:
+	sleepTime = 0.5
+	def __init__(self, sleepTime = 0.5):
+		print("Blink initialized")
+		self.sleepTime = sleepTime
 
-from time import sleep
+	def setSleepTime(self, time):
+		self.sleepTime = time
 
-GPIO.setmode(GPIO.BOARD)
-pins=[36, 38]
-GPIO.setup(pins, GPIO.OUT)
-sleepTime = 0.5
-while True:
-	GPIO.output(36, 1)
-	GPIO.output(38, 0)
-	sleep(sleepTime)
-	GPIO.output(36, 0)
-	GPIO.output(38, 1)
-	sleep(sleepTime)
-	print(sleepTime)
-GPIO.cleanup(pins)
+	def getSleepTime(self):
+		print(self.sleepTime)
+	def main(self):
+		try:
+			import RPi.GPIO as GPIO
+		except RuntimeError:
+			print("...error importing RPi.GPIO module")
+		from time import sleep
+
+		GPIO.setmode(GPIO.BOARD)
+		pins=[36, 38]
+		GPIO.setup(pins, GPIO.OUT)
+		while True:
+			GPIO.output(36, 1)
+			GPIO.output(38, 0)
+			sleep(self.sleepTime)
+			GPIO.output(36, 0)
+			GPIO.output(38, 1)
+			sleep(self.sleepTime)
+		print(getSleepTime())
+	def stop(self):
+		GPIO.cleanup(pins)
+
+if __name__ == "__main__":
+	Blinker = Blinker()
+	Blinker.getSleepTime()
+	Blinker.stop()
+
